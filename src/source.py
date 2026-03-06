4a335b2e0bd6102b1cb5de7d84b93be573826756
futo <fedor.alekseev.msk@gmail.com>
2026-03-06 13:59:22 +0300

    Add generator and file sources

---
+++ src/source.py
@@ -0,0 +1,67 @@
+from src.task import Task
+from os import path
+import hashlib
+import logging
+logger = logging.getLogger(__name__)
+
+
+'''class APISource:
+    def get_tasks(self) -> list[Task]:
+'''
+
+
+class FileSource:
+    def __init__(self, file_dir: str):
+        if not path.isfile(file_dir):
+            logger.info(f"Failed to open {file_dir}")
+            raise ValueError("File was not found")
+        self.file = path.abspath(file_dir)
+
+    def get_tasks(self) -> list[Task]:
+        logging.basicConfig(filename='filesrc.log', level=logging.INFO)
+        tasks = []
+        try:
+            with open(self.file, encoding="utf-8") as f:
+                for line in f:
+                    id, description = line.split(maxsplit=1)
+                    current_task = Task(
+                        id=f"task_{id}",
+                        payload={
+                            "source": "file",
+                            "path": self.file,
+                            "description": description,
+                        }
+                    )
+                    tasks.append(current_task)
+                    logger.info(f"Initiated {current_task.id} from file located in \"{self.file}\"")
+
+        except PermissionError as e:
+            logger.info(f"Failed to read {self.file}: {e}")
+            raise ValueError("Failed to read file due to permission")
+
+        except UnicodeDecodeError as e:
+            logger.info(f"Failed to read {self.file}: {e}")
+            raise ValueError("Failed to decode file")
+
+        except ValueError as e:
+            logger.info(f"File does not contain correct info")
+            raise ValueError(f"Bad line: {e}")
+
+class RandomSource:
+    def __init__(self, amount: int = 5):
+        self.amount = amount
+
+    def get_tasks(self) -> list[Task]:
+        logging.basicConfig(filename='rndsrc.log', level=logging.INFO)
+        tasks = []
+        for i in range(1, self.amount + 1):
+            current_task = Task(
+                id=f"task_{i}",
+                payload={
+                    "source": "generator",
+                    "hash": hashlib.sha256(f"GENERATORTASK_{i}".encode()).hexdigest(),
+                }
+            )
+            tasks.append(current_task)
+            logger.info(f"Initiated {current_task.id} with hash \"{current_task.payload['hash']}\" through generator")
+        return tasks
